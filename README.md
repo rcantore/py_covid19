483b7d437abd981f50282c8151cbfde0c4e1ec1e
Roberto Cantore <rcantore@gmail.com>
2020-03-23 14:05:46 -0300

    Initial import

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple Covid-19 Python tracker
+This is a work in progress
+
+### Dependencies
+
+```
+Python 3
+```
+
+### TO-DO
+Feature: filtering countries (right now, only Argentina is supported)
+Validate the information source:
+* https://github.com/NovelCOVID/API
+* https://www.worldometers.info/coronavirus/
---
+++ covidtracker.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import io
+import time
+import json
+from urllib.request import Request, urlopen
+from http.client import HTTPResponse
+from os import system, name 
+
+def clear(): 
+    #windows 
+    if name == 'nt': 
+        _ = system('cls') 
+    #linux o mac
+    else: 
+        _ = system('clear')
+
+def call_web():
+    req = Request('https://corona.lmao.ninja/countries/Argentina', headers={'User-Agent': 'Mozilla/5.0'})
+    with urlopen(req) as response:
+        jsonResponse = json.load(response)
+
+        print("{:^20s}|{:^16s}|{:^16s}|{:^16s}|".format("Pais","Total Casos","Nuevos Casos","Fallecimientos"))
+        print("{:^20s}|{:^16d}|{:^16d}|{:^16d}|".format(jsonResponse["country"],jsonResponse["cases"],jsonResponse["todayCases"],jsonResponse["deaths"]))
+
+clear()
+call_web()
+while True:
+    #Espera larga para no cargar servidor 6 horas
+    time.sleep(21600)
+    clear()
+    call_web()
