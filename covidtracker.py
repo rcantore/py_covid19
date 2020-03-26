fd1f5729d684cad97ad4fe87c1666f8917f0f357
Roberto Cantore <rcantore@gmail.com>
2020-03-26 11:04:15 -0300

    better error handling

--- covidtracker.py
+++ covidtracker.py
@@ -4,7 +4,7 @@ import time
 import json
 import sys
 from urllib.request import Request, urlopen, HTTPError
-from http.client import HTTPResponse
+from http.client import HTTPResponse, InvalidURL
 from os import system, name 
 
 def clear(): 
@@ -22,9 +22,9 @@ def call_web(country):
         with urlopen(req) as response:
             jsonResponse = json.load(response)
 
-            print("{:^20s}|{:^16s}|{:^16s}|{:^16s}|{:^16s}|".format("Country","Confirmed Cases","Today Cases","Recovered","Deaths"))
-            print("{:^20s}|{:^16d}|{:^16d}|{:^16d}|{:^16d}|".format(jsonResponse["country"],jsonResponse["cases"],jsonResponse["todayCases"],jsonResponse["recovered"],jsonResponse["deaths"]))
-    except HTTPError as err:
+            print("{:^20s}|{:^17s}|{:^16s}|{:^16s}|{:^14s}|".format("Country","Confirmed Cases","Today Cases","Recovered","Deaths"))
+            print("{:^20s}|{:^17d}|{:^16d}|{:^16d}|{:^14d}|".format(jsonResponse["country"],jsonResponse["cases"],jsonResponse["todayCases"],jsonResponse["recovered"],jsonResponse["deaths"]))
+    except (HTTPError, InvalidURL) as err:
         print ("Something went wrong. Please check the Country name you introduced")
         sys.exit()
 
@@ -33,6 +33,7 @@ if __name__ == "__main__":
     country = "Argentina"
     arguments = len(sys.argv) - 1
     if arguments > 0:
+        #TODO encode parameter 
         country = sys.argv[1]
 
 
